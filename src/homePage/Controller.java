35e506f3993aba6f0e39b5c39d904868fd8e9ccf
Jeric Ortillo <jericortillo@gmail.com>
2017-11-02 17:49:39 -0700

    Auto stash before merge of "master" and "origin/master"
    Scene builder fixed. (Stage somehow fucked it up) Added open as menu item. Textfield sets text to file path. Added import button. (Just a placeholder, could be used to upload file to database.)

--- src/homePage/Controller.java
+++ src/homePage/Controller.java
@@ -1,6 +1,57 @@
 package homePage;
+import java.io.File;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.event.ActionEvent;
 
-public class Controller
-{
+import java.io.File;
 
+public class Controller {
+   @FXML
+    private TextField directoryTextField;
+  /*  public Button browse;
+
+    public MenuItem openFile;
+*/
+    @FXML
+    void openAction(ActionEvent event) {
+        handleOpenClick();
+    }
+
+    public void handleOpenClick() {
+        System.out.print("Fuck you... STFU");
+
+        //openFile.setOnAction(new EventHandler<ActionEvent>());
+        //Browse function
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("Open Excel File");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Excel Files", "*.xlsx", "*.xls"));
+
+        File chosenExcelDocs = fileChooser.showOpenDialog(null);
+        //check to see if chosenExcelDocs is populated or not
+        if (chosenExcelDocs != null) {
+            //directoryTextField.setText(fileChooser.getName());
+            //mainStage.display(selectedFile);
+           directoryTextField.setText(chosenExcelDocs.getPath());
+        }
+
+   /* public void onButtonClick() {
+        System.out.print("Fuck you");
+
+        //Browse function
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Excel File");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Excel Files", "*.xlsx", "*.xls"));
+
+        File chosenExcelDocs = fileChooser.showOpenDialog(null);
+
+        //check to see if chosenExcelDocs is populated or not
+        if (chosenExcelDocs != null) {
+            //mainStage.display(selectedFile);
+        }*/
+    }
 }
--- src/homePage/Main.java
+++ src/homePage/Main.java
@@ -4,20 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import sun.plugin.javascript.navig.Anchor;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //Why Parent? why not "stage"
+       // Stage stage = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+        AnchorPane root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+        // primaryStage.setTitle("Budget Application");
+        // primaryStage.setScene(new Scene(stage, 300, 275));
+        //stage.show();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Budget Application");
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
